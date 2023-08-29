51a0dcd1a5b3fef765da357b4eeb28b25117f052
sergej.ruff <Sergej.Ruff@stud.mh-hannover.de>
2023-08-29 10:29:56 +0200

    added extraction of 5 and 3 utrs

--- ORF_pipeline.R
+++ ORF_pipeline.R
@@ -5,7 +5,7 @@ rm(list=ls())
 library(traits)
 library(seqinr)
 library(ORFik)
-
+library(dplyr)
 
 # Import from local files
 sequences <- read.fasta(file="A:/Praktikum_Chris/data/Covid_ref/sequence.fasta",as.string=TRUE)
@@ -16,10 +16,32 @@ sequences <- sequences[[1]]
 # extract the nucleotide sequence
 seq <- sequences[1]
 
+# find the ORFs in + direction with length of 300 or more. Using ORFik-package
 orfs<- findORFs(seq, minimumLength = 98)
 
-orf_matrix <- as.matrix(orfs@unlistData)
+print(orfs)
 
-orf_matrix <- cbind(orf_matrix,orf_matrix[,1]+orf_matrix[,2])
+# Convert the Data to a data frame
+orf_df <- as.data.frame(orfs@unlistData)
 
-colnames(orf_matrix)<- c("start","length","end")
+# extract last position of 5´-UTR
+firstposition <- min(orf_df$start)-1
+
+# extract first position of 3´-UTR
+lastposition <- max(orf_df$end)+1
+
+cat(paste("5´-UTR goes from position 1-", firstposition,
+            "\n3´-UTR starts at position:", lastposition,
+            "and ends with the last nucleotide"))
+
+
+# extract 5´- and 3´-UTR
+five_UTR <- substring(seq, first = 1, last=firstposition)
+three_UTR <- substring(seq, first =lastposition )
+
+#RNAstructure requires nucleotides to be upper case
+five_UTR <- toupper(five_UTR)
+
+three_UTR <- toupper(three_UTR)
+
+
