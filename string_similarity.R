281fae7775a009b785526e7034c70bebcf615117
sergej.ruff <Sergej.Ruff@stud.mh-hannover.de>
2023-08-30 13:46:16 +0200

    changes were made

--- string_similarity.R
+++ string_similarity.R
@@ -1,30 +1,123 @@
 # Load the required packages
 library(Biostrings)
 library(stringdist)
+library(DECIPHER)
+library(msa)
+library(seqinr)
+
+alignment_results_local <- list()
+alignment_results_global <- list()
+for (i in 1:length(results_list)){
+  for(j in 1:length(results_list)){
+    if(i!=j){
+      # Example dot-bracket notations from your results_list
+      sequence_1 <- results_list[[i]][2,]
+      sequence_2 <- results_list[[j]][2,]
+
+      # Perform pairwise alignment
+      alignment <- pairwiseAlignment(pattern = sequence_1, subject = sequence_2,type="local")
+
+      lokal_seq <- c(alignedPattern(alignment), alignedSubject(alignment))
+      #BrowseSeqs(lokal_seq)
+      alignment_results_local[[paste("alignment_", i, "_", j, sep = "")]] <- alignment
+
+      alignment_g <- pairwiseAlignment(pattern = sequence_1, subject = sequence_2,type="global")
+      global_seq <- c(alignedPattern(alignment_g ), alignedSubject(alignment_g ))
+      #BrowseSeqs(global_seq)
+      alignment_results_global[[paste("alignment_", i, "_", j, sep = "")]] <- alignment_g
+    }
+
+  }
+
+}
+
+
+#####################################################################
+
+
+# Create an empty data frame to store results
+results_df_lev <- data.frame(name1 = character(0),
+                         name2 = character(0),
+                         lev_distance = numeric(0),
+                         lev_distance_percentage = numeric(0))
+for (i in 1:length(results_list)){
+  for(j in 1:length(results_list)){
+    if(i!=j){
+
+      # Example dot-bracket notations from your results_list
+      sequence_1 <- results_list[[i]][2,]
+      sequence_2 <- results_list[[j]][2,]
+
+      name1 <- names(results_list)[i]
+      name2 <- names(results_list)[j]
+
+      # Calculate the Levenshtein distance
+      lev_distance <- stringdist::stringdist(sequence_1, sequence_2)
+
+      cat("Levenshtein Distance:", lev_distance, "\n")
+      # shows how many edits need to be taken to transform one sequence into another
+
+      # Length of the longer sequence
+      max_length <- max(nchar(sequence_1), nchar(sequence_2))
+
+      # Calculate the Levenshtein distance as a percentage
+      lev_distance_percentage <- (lev_distance / max_length) * 100
+
+      # Add data to the data frame
+      results_df_lev <- rbind(results_df_lev, data.frame(name1, name2, lev_distance, lev_distance_percentage))
+    }
+
+  }
+
+}
+
+# Print the results data frame
+View(results_df_lev)
+
+##############################################################################
+# Extract the second row from each data frame
+#dot_sequences <- lapply(results_list, function(df) df[1, ])
+
+
+#aa_sequences <- AAStringSet(unlist(dot_sequences))
+#testmsa <- msa(aa_sequences)
+#print(testmsa, show="complete")
+
+
+
+###################################################
+#####################################################################
 
 # Example dot-bracket notations from your results_list
-sequence_1 <- results_list[[3]][2,]
-sequence_2 <- results_list[[4]][2,]
+#sequence_1 <- results_list[[5]][2,]
+#sequence_2 <- results_list[[6]][2,]
 
 # Perform pairwise alignment
-alignment <- pairwiseAlignment(pattern = sequence_1, subject = sequence_2,type="local")
+#alignment <- pairwiseAlignment(pattern = sequence_1, subject = sequence_2,type="local")
 
+#lokal_seq <- c(alignedPattern(alignment), alignedSubject(alignment))
+#BrowseSeqs(lokal_seq)
 
-alignment_g <- pairwiseAlignment(pattern = sequence_1, subject = sequence_2,type="global")
+#alignment_g <- pairwiseAlignment(pattern = sequence_1, subject = sequence_2,type="global")
+#global_seq <- c(alignedPattern(alignment_g ), alignedSubject(alignment_g ))
+#BrowseSeqs(global_seq)
 
 ###############
+###################################
+# Print the results data frame
+#View(results_df_lev)
 
 # Calculate the Levenshtein distance
-lev_distance <- stringdist::stringdist(sequence_1, sequence_2)
+#lev_distance <- stringdist::stringdist(sequence_1, sequence_2)
 
-cat("Levenshtein Distance:", lev_distance, "\n")
+#cat("Levenshtein Distance:", lev_distance, "\n")
 # shows how many edits need to be taken to transform one sequence into another
 
 # Length of the longer sequence
-max_length <- max(nchar(sequence_1), nchar(sequence_2))
+#max_length <- max(nchar(sequence_1), nchar(sequence_2))
 
 # Calculate the Levenshtein distance as a percentage
-lev_distance_percentage <- (lev_distance / max_length) * 100
+#lev_distance_percentage <- (lev_distance / max_length) * 100
 
-cat("Levenshtein Distance Percentage:", lev_distance_percentage, "%\n")
+#cat("Levenshtein Distance Percentage:", lev_distance_percentage, "%\n")
 # what percentage of dots and points needs to be changed in the longer sequence to match teh short sequence.
