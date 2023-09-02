700668136e886a8a7cf46b3f1666554479b0d389
sergej.ruff <Sergej.Ruff@stud.mh-hannover.de>
2023-09-02 02:12:19 +0200

    good demonstration on how to fill matrix diagonally with nussinov

--- nussinov_algo_demonstration.R
+++ nussinov_algo_demonstration.R
@@ -1,9 +1,6 @@
 
-sequence <- c(1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15)
-
-
+sequence <- c(1, 2, 3, 4, 5)
 n <- length(sequence)
-
 for (l in 2:n) {
   for (i in 1:(n - l + 1)) {
     j <- i + l - 1
